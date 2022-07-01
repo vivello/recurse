e7215960355e1f04f4acf0aa8a58c05868380f2c
vivello <56002394+vivello@users.noreply.github.com>
2022-07-01 03:13:09 -0400

    Add show_board capability and make functional

--- ttt.py
+++ ttt.py
@@ -1,43 +1,69 @@
 # tic-tac-toe game
 
-SIZE = 3
-MIN_TURN = SIZE * 2 - 1
+import copy
 
-def check_lines(board):
+
+def check_lines(board, size):
     for row in board:
-        if row.count("X") == SIZE:
+        if row.count("X") == size:
             return "X"
-        elif row.count("O") == SIZE:
+        elif row.count("O") == size:
             return "O"
     return "None"
 
-def flip_board(board):
+def flip_board(board, size):
     flipped = []
-    for i in range(SIZE):
+    for i in range(size):
         new_row = []
         for row in board:
             new_row.append(row[i])
         flipped.append(new_row)
     return flipped
 
-def diags(board):
+def diags(board, size):
     lines = [[],[]]
-    for i in range(SIZE):
+    for i in range(size):
            lines[0].append(board[i][i])
            lines[1].append(board[i][-i - 1])
     return lines
+
+def gen_board(size):
+    board = []
+    empty_row = [" "] * size
+    for i in range(size):
+        board.append(copy.deepcopy(empty_row))
+    return board
         
 
 class TTT:
     # TTT() produces a new tic-tac-toe board and game state
     # __init__: -> TTT
-    def __init__(self):
-        self._board = [[" "] * SIZE] * SIZE
+    def __init__(self, board_size=3):
+        self._size = board_size
+        self._board = gen_board(self._size)
         self._player = "X"
         self._turn = 0
         self._winner = ""
 
-    #
+    # self.show_board() produces a string representation of the tic-tac-toe board
+    def show_board(self):
+        board = "    "
+        line = ""
+        for i in range(self._size):
+            line += str(i) + " " * 3
+        board += line + "\n\n"
+        for i in range(self._size):
+            line = str(i) + "  "
+            for j in range(self._size):
+                if j < (self._size - 1):
+                    line += " " + self._board[i][j] + " |"
+                else:
+                    line += " " + self._board[i][j] + " \n"
+            if i < (self._size - 1):
+                line += "   " + ("--- " * self._size) + "\n"
+            board += line
+        return board 
+    
     '''
         0   1   2
          
@@ -46,13 +72,14 @@ class TTT:
     1   O | X | O
        --- --- ---
     2   X | O | X
+    
     '''
 
     # self.add(self, col, row) enters the player's move into the board if it is valid
     def add(self, col, row):
-        if col < 0 or col > (SIZE - 1):
+        if col < 0 or col > (self._size - 1):
             print("Please enter a valid square selection")
-        elif row < 0 or row > (SIZE - 1):
+        elif row < 0 or row > (self._size - 1):
             print("Please enter a valid square selection")
         else:
             if self._board[row][col] == " ":
@@ -62,13 +89,14 @@ class TTT:
                     self._player = "O"
                 else:
                     self._player = "X"
+                print(self._board)
             else:
                 print("Please select an empty square")
                 
 
     # self.check() determines if a game has concluded
     def check(self):
-        if self._turn < MIN_TURN:
+        if self._turn < (self._size * 2 - 1):
             return False
         else:
             to_check = [self._board,flip_board(self._board),diags(self._board)]
@@ -76,7 +104,7 @@ class TTT:
             if len(checked) > 0:
                 self._winner = checked[0]
                 return True
-            elif self._turn == (SIZE * SIZE - 1) and len(checked) == 0:
+            elif self._turn == (self._size * self._size - 1) and len(checked) == 0:
                 self._winner = "Tie"
                 return True
             else:
