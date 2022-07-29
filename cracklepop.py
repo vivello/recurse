eb2c7b89c68a5dec40a35fbc49cabed56670678c
vivello <56002394+vivello@users.noreply.github.com>
2022-07-29 09:18:54 -0400

    Missed a space

--- cracklepop.py
+++ cracklepop.py
@@ -6,7 +6,7 @@
 def not_fizzbuzz():
     for i in range(1,101):
         word = ''
-        if (i %3) == 0:
+        if (i % 3) == 0:
             word += 'Crackle'
         if (i % 5) == 0:
             word += 'Pop'
