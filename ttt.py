a5e898a11ca4446c1b9a14b7ba9dd276fed9af7f
vivello <56002394+vivello@users.noreply.github.com>
2022-07-29 09:14:43 -0400

    Just make it playable

--- ttt.py
+++ ttt.py
@@ -2,7 +2,11 @@
 
 import copy
 
+# *********
+# HELPERS
+# *********
 
+# check_lines(board, size) returns which player has completed a row on the board, if any
 def check_lines(board, size):
     for row in board:
         if row.count("X") == size:
@@ -11,6 +15,7 @@ def check_lines(board, size):
             return "O"
     return "None"
 
+# flip_board(board, size) inverts the rows and columns of a board
 def flip_board(board, size):
     flipped = []
     for i in range(size):
@@ -20,6 +25,7 @@ def flip_board(board, size):
         flipped.append(new_row)
     return flipped
 
+# diags(board, size) represents the diagonals of a board as rows
 def diags(board, size):
     lines = [[],[]]
     for i in range(size):
@@ -27,12 +33,19 @@ def diags(board, size):
            lines[1].append(board[i][-i - 1])
     return lines
 
+# gen_board(size) generates a tic tac toe board where the dimensions are size * size
 def gen_board(size):
     board = []
     empty_row = [" "] * size
     for i in range(size):
         board.append(copy.deepcopy(empty_row))
     return board
+
+
+
+# ************
+# CLASS STUFF
+# ************
         
 
 class TTT:
@@ -76,35 +89,42 @@ class TTT:
     '''
 
     # self.add(self, col, row) enters the player's move into the board if it is valid
-    def add(self, col, row):
-        if col < 0 or col > (self._size - 1):
-            print("Please enter a valid square selection")
-        elif row < 0 or row > (self._size - 1):
-            print("Please enter a valid square selection")
-        else:
-            if self._board[row][col] == " ":
-                self._board[row][col] = self._player
-                self._turn += 1
-                if self._player == "X":
-                    self._player = "O"
-                else:
-                    self._player = "X"
-                print(self._board)
+    def add(self):
+        while True:
+            col, row = input("Enter the column and then the row number for your square selection separated by a space: ").split()
+            col = int(col)
+            row = int(row)
+            if col < 0 or col > (self._size - 1):
+                print("Please enter a valid column selection")
+                continue
+            elif row < 0 or row > (self._size - 1):
+                print("Please enter a valid row selection")
+                continue
             else:
-                print("Please select an empty square")
-                
+                if self._board[row][col] == " ":
+                    self._board[row][col] = self._player
+                    self._turn += 1
+                    if self._player == "X":
+                        self._player = "O"
+                    else:
+                        self._player = "X"
+                    print(self._board)
+                    break
+                else:
+                    print("Please select an empty square")
+                                       
 
     # self.check() determines if a game has concluded
     def check(self):
         if self._turn < (self._size * 2 - 1):
             return False
         else:
-            to_check = [self._board,flip_board(self._board),diags(self._board)]
-            checked = list(filter(lambda x: False if x == "None" else True, map(check_lines, to_check)))
+            to_check = [self._board,flip_board(self._board, self._size),diags(self._board, self._size)]
+            checked = list(filter(lambda x: False if x == "None" else True, map(lambda y: check_lines(y, self._size), to_check)))
             if len(checked) > 0:
                 self._winner = checked[0]
                 return True
-            elif self._turn == (self._size * self._size - 1) and len(checked) == 0:
+            elif self._turn == (self._size * self._size) and len(checked) == 0:
                 self._winner = "Tie"
                 return True
             else:
@@ -119,18 +139,25 @@ class TTT:
         return self._player
 
 
+
+# *************
+# GAME WRAPPER
+# *************
+
 def play_game():
     game = ""
     game_type = ""
     print("Welcome to TIC TAC TOE")
-    while (game_type != "Y") or (game_type != "C"):
+    while True:
         print("If you would like to play classic tic tac toe, enter Y.")
         print("Alternatively, for a custom game, please enter C.")
         game_type = input("Y / C: ")
         if game_type == "Y":
             game = TTT()
+            break
         elif game_type == "C":
-            while isinstance(current_game, str)
+            print("here")
+            while isinstance(game, str):
                 size_input = input("Please enter the size of your desired custom board: ")
                 try:
                     game_size = int(size_input)
@@ -141,8 +168,23 @@ def play_game():
                         print("Board size must be a positive integer value. Try again.")
                     else:
                         game = TTT(game_size)
+                        break
+            break
         else:
             print("That is not a valid selection. Try again.")
+
     while game.winner() == "":
         print(game.show_board())
+        print("It is {}'s turn".format(game.player()))
+        game.add()
+        game.check()
+
+    print(game.show_board())
+    if game.winner() == "Tie":
+        print("Tie game!")
+    else:
+        print(game.winner() + " wins!")
+    print("GAME OVER")
+
+play_game()        
             
